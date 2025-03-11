765822061d99ebf46839412fdbe11daa2528d56f
Leandro José <ljanotte@gmail.com>
2025-03-11 20:59:19 -0300

    Creating azarado

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import carregarPessoas from './function/carregar-pessoas';
+import sortearAzarado from './function/sortear-azarado';
 
 const pessoas = carregarPessoas('src/data/dados.csv');
+// pessoas.forEach((p) => console.log(p.toString()));
 
-pessoas.forEach((p) => console.log(p.toString()));
+const azarado = sortearAzarado(pessoas);
+console.log(azarado.toString());
